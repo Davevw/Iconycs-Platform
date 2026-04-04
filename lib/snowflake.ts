d67ded96ab8eb8d6c07187d56580e3078e1f399c
Davevw <dave@wrfco.com>
2026-04-04 11:22:56 -0700

    fix: revert Snowflake role to PUBLIC (works for all views)

--- lib/snowflake.ts
+++ lib/snowflake.ts
@@ -16,7 +16,7 @@ const connectionConfig = {
   warehouse: process.env.SNOWFLAKE_WAREHOUSE || 'QRY_WAREHOUSE',
   database: process.env.SNOWFLAKE_DATABASE || 'PROPERTYANALYTICS',
   schema: process.env.SNOWFLAKE_SCHEMA || 'PUBLIC',
-  role: process.env.SNOWFLAKE_ROLE || 'SYSADMIN',
+  role: process.env.SNOWFLAKE_ROLE || 'PUBLIC',
   clientSessionKeepAlive: true,
   clientSessionKeepAliveHeartbeatFrequency: 3600,
 };
